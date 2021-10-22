c7221d9ba68cea0f468f038ee5dbba9e99b4e625
nick adams <thebigdeal916@icloud.com>
2021-10-22 15:37:59 -0500

    fixed nextline issue

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately: %s", pi);
+        System.out.printf("The value of pi is approximately: %s \n", pi);
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a number please: ");
         int userInput = scanner.nextInt();
-        System.out.println("The number you entered was: " + userInput);
+        System.out.printf("The number you entered was: %s \n",  userInput);
 
         System.out.print("enter three words: ");
         String userInput1 = scanner.next();
@@ -17,14 +17,15 @@ public class ConsoleExercises {
         System.out.println("The three words you entered were: " + userInput1 + " " + userInput2 + " " + userInput3);
 
 
-//        System.out.print("enter a sentence: ");
-//        String userInput4 = scanner.nextLine();
-//        System.out.printf("Your sentence is: %s", userInput4);
+        System.out.print("enter a sentence: \n");
+        scanner.nextLine();
+        String userInput4 = scanner.nextLine();
+        System.out.printf("Your sentence is: %s \n", userInput4);
 
         System.out.print("Enter a length and width value: ");
-        int input1 = scanner.nextInt();
-        int input2 = scanner.nextInt();
-        System.out.println("The area of the room would be: " + (input1 * input2) + " The perimeter of this room would be " + ((input1 * input1) + (input2 * input2)));
+        float input1 = scanner.nextFloat();
+        float input2 = scanner.nextFloat();
+        System.out.println("The area of the room would be: " + (input1 * input2) + ", The perimeter of this room would be " + ((input1 * input1) + (input2 * input2)));
 
 
 
