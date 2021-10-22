8c24b0faed4211f195754b50fd939c06e301d39d
nick adams <thebigdeal916@icloud.com>
2021-10-22 09:08:09 -0500

    added int and string variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
         System.out.print("World!");
+
+
+        int myFavoriteNumber = 40;
+        System.out.println(myFavoriteNumber);
+        String myString = "this is my string!";
+        System.out.println(myString);
     }
 }
