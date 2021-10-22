26ac059629b65b41a893a4c1054ed64a093a47e1
nick adams <thebigdeal916@icloud.com>
2021-10-22 09:47:59 -0500

    added long variable

--- src/SyntaxLecture.java
+++ src/SyntaxLecture.java
@@ -2,9 +2,23 @@ public class SyntaxLecture {
 
     public static void main(String[] args) {
         System.out.println("And \\ or \\ for \\ \n nor \\ but \\ yet \\ so");
-
+        // constant variable
         final int SIDES_OF_A_DICE = 6;
         System.out.println("how many sides on my dice? " + SIDES_OF_A_DICE);
+
+        // Basic operators
+        int a = 6;
+        // unary
+        a++;
+        // binary
+        a = a + 4;
+        System.out.println(a);
+
+        // casting
+        int myInteger = 900;
+        long morePrecise = myInteger;
+
+        System.out.println("This is a long: " + (morePrecise + 1000000000L));
 
     }
 }
