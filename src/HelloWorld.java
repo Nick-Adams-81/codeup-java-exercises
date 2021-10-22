47791a976d21830276cb641920542383e800a9b3
nick adams <thebigdeal916@icloud.com>
2021-10-22 09:23:00 -0500

    syntax lecture init push

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,5 +8,22 @@ public class HelloWorld {
         System.out.println(myFavoriteNumber);
         String myString = "this is my string!";
         System.out.println(myString);
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+        int three = (int) o;
+
+
     }
 }
---
+++ src/SyntaxLecture.java
@@ -0,0 +1,6 @@
+public class SyntaxLecture {
+
+    public static void main(String[] args) {
+        System.out.print("And \\ or \\ for \\ \n nor \\ but \\ yet \\ so");
+    }
+}
