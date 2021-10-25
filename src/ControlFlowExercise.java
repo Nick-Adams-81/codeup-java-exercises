ccac7217eb8f497c1ca3303cc1fe798adad5856a
nick adams <thebigdeal916@icloud.com>
2021-10-25 11:46:18 -0500

    added confirm to powers tables

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -4,25 +4,25 @@ public class ControlFlowExercise {
 
     public static void main(String[] args) {
 
-        int i = 5;
-        while (i <= 15) {
-            System.out.println("i: " + i);
-            i++;
-        }
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println("i: " + i);
+//            i++;
+//        }
+
+//
+//        int j = 0;
+//        do {
+//            System.out.println("The count is: " + (j));
+//            j += 2;
+//        } while(j <= 100);
 
 
-        int j = 0;
-        do {
-            System.out.println("The count is: " + (j));
-            j += 2;
-        } while(j <= 100);
-
-
-        int k = 100;
-        do {
-            System.out.println("K:" + k);
-            k -= 5;
-        } while(k >= 0);
+//        int k = 100;
+//        do {
+//            System.out.println("K:" + k);
+//            k -= 5;
+//        } while(k >= 0);
 
 
         long l = 2;
@@ -32,30 +32,40 @@ public class ControlFlowExercise {
         } while(l < 10000000);
 
 
-        for (int y = 1; y <= 100; y++) {
-            if(y % 15 == 0) {
-                System.out.println("Fizz Buzz");
-            } else if (y % 3 == 0) {
-                System.out.println("Fizz");
-            } else if(y % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(y);
-            }
-        }
+//        for (int y = 1; y <= 100; y++) {
+//            if(y % 15 == 0) {
+//                System.out.println("Fizz Buzz");
+//            } else if (y % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if(y % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(y);
+//            }
+//        }
 
 
         Scanner scanner = new Scanner(System.in);
 //
-        System.out.print("Enter a number please: ");
-        int userNum = scanner.nextInt();
-        System.out.println(" Here is your table!");
-        System.out.println("");
-        System.out.println("number  |  squared  |  cubed ");
-        System.out.println("------- |  -------  |  -----");
-        for (int z = 1; z <= userNum; z++) {
-            System.out.println(z + "       |  " + (z * z) + "        |  "+ (z * z * z));
-        }
+
+
+        boolean userConfirm = true;
+        do {
+            System.out.print("Enter a number please: ");
+            int userNum = scanner.nextInt();
+            System.out.println(" Here is your table!");
+            System.out.println("");
+            System.out.println("number  |  squared  |  cubed ");
+            System.out.println("------- |  -------  |  -----");
+            for (int z = 1; z <= userNum; z++) {
+                System.out.println(z + "       |  " + (z * z) + "        |  "+ (z * z * z));
+            }
+            System.out.println("Continue? [y/n]");
+            String response3 = scanner.next();
+            userConfirm = response3.equals("y");
+
+        } while(userConfirm);
+
 //
 
         boolean confirm = true;
