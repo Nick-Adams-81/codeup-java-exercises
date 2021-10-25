ea6326e8fa32c9263cf20391cea47400da8ea393
nick adams <thebigdeal916@icloud.com>
2021-10-25 09:02:45 -0500

    added do while loop

--- src/ControlFlowExercise.java
+++ src/ControlFlowExercise.java
@@ -7,5 +7,26 @@ public class ControlFlowExercise {
             System.out.println("i: " + i);
             i++;
         }
+
+
+        int j = 0;
+        do {
+            System.out.println("The count is: " + (j));
+            j += 2;
+        } while(j <= 100);
+
+
+        int k = 100;
+        do {
+            System.out.println("K:" + k);
+            k -= 5;
+        } while(k >= 0);
+
+
+        int l = 2;
+        do {
+            System.out.println("Count: " + l);
+            l *= 2;
+        } while(l < 1000000);
     }
 }
