b318ef8a60da21a05c9d6671065762ac8be061dc
nick adams <thebigdeal916@icloud.com>
2021-10-26 10:31:50 -0500

    added do while loop for bob

--- src/Bob.java
+++ src/Bob.java
@@ -4,23 +4,33 @@ import java.util.Scanner;
 public class Bob {
 
     public static void main(String[] args) {
+        // initializing a scanner
         Scanner scanner = new Scanner(System.in);
 
 
-
+        boolean confirm;
+        do {
             System.out.println("Say something...");
             String input = scanner.nextLine();
             System.out.printf("You said %s \n", input);
 
-            if(input.endsWith("!")) {
+            // checking the user input and sending a response based on the user input
+            if (input.endsWith("!")) {
                 System.out.println("Whoa, chill out!");
-            } else if(input.endsWith("?")) {
+            } else if (input.endsWith("?")) {
                 System.out.println("Sure.");
-            } else if(input.equals("")) {
+            } else if (input.equals("")) {
                 System.out.println("Fine, Be that way!");
             } else {
                 System.out.println("Whatever");
             }
+
+            System.out.println("Continue? [y/n]");
+            String response = scanner.nextLine();
+            confirm = response.equalsIgnoreCase("y");
+
+        } while(confirm);
+        // asking the user for some input
 
 
     }
