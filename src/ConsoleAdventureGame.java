61c71bdbb85580d2d0ab940fe3d3e695f8e60e41
nick adams <thebigdeal916@icloud.com>
2021-10-28 10:04:34 -0500

    added first object

--- src/ConsoleAdventureGame.java
+++ src/ConsoleAdventureGame.java
@@ -22,27 +22,32 @@ public class ConsoleAdventureGame {
             System.out.println("Tell me your name");
             String name = scanner.nextLine();
             System.out.println("Hello " + name + ", nice to meet you!");
-            int heroAttack = randomInt(5,  50);
-            int enemyAttack = randomInt(15, 25);
+
             System.out.println("Your health is " + heroHealth + ", the enemy has " + enemyHealth + " health left, do you wish to attack?[y/n]");
 
                 String action = scanner.nextLine();
                 if (action.equalsIgnoreCase("y")) {
                     boolean attack;
                     do{
+                        int heroAttack = randomInt(5,  50);
+                        int enemyAttack = randomInt(15, 25);
                         heroHealth -= enemyAttack;
                         enemyHealth -= heroAttack;
-                        System.out.println("Your attack dealt " + heroAttack + " damage, your enemys health is " + (enemyHealth   + " however your enemy dealt " + enemyAttack + " damage to you, leaving you with " + (heroHealth)));
+                        System.out.println(name + ", Your attack dealt " + heroAttack + " damage, your enemys health is " + (enemyHealth   + " however your enemy dealt " + enemyAttack + " damage to you, leaving you with " + (heroHealth)));
                         if(enemyHealth <= 0) {
-                            System.out.println("You killed the enemy!");
+                            System.out.println(name + ", You killed the enemy!");
                         } else if(heroHealth <= 0) {
                             System.out.println("You died!");
                         }
 
-                        System.out.println("Do you wish to continue the attack or run away?? [attack/run]");
+                        System.out.println(name + ", Do you wish to continue the attack, take a potion, or run away?? [attack/potion/run]");
                         String response = scanner.nextLine();
-                        attack = response.equalsIgnoreCase("attack");
+                        if(response.equalsIgnoreCase("potion")) {
+                            heroHealth += 10;
+                            System.out.println("You took the potion, your health is now " + (heroHealth));
+                        }
 
+                        attack = response.equalsIgnoreCase("attack");
 
                     } while(attack);
 
---
+++ src/Java2Lecture.java
@@ -0,0 +1,20 @@
+
+
+public class Java2Lecture {
+
+
+
+    public static void main(String[] args) {
+
+        class Person {
+            public String firstName;
+            public String lastName;
+        }
+
+        Person rick = new Person();
+        rick.firstName = "Rick";
+        rick.lastName = "Sanchez";
+
+
+    }
+}
