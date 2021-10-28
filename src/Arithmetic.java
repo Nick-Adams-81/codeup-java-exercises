8fda8a8fb9203bc8e36474283dddafdcb3bc14cf
nick adams <thebigdeal916@icloud.com>
2021-10-28 10:38:55 -0500

    added math object

---
+++ src/Arithmetic.java
@@ -0,0 +1,24 @@
+public class Arithmetic {
+
+    // static fields
+    // a static property:
+    public static double pi = 3.14159;
+
+    // static methods
+    public static int add( int x, int y) {
+        return x + y;
+    }
+
+    public static void main(String[] args) {
+        // do not go down this road
+//        Arithmetic myMath = new Arithmetic();
+//        System.out.print( myMath.pi);
+        // still works but incorrect
+
+        System.out.println(Arithmetic.pi);
+        System.out.println(Arithmetic.add(4, 6));
+        // built in Math object
+        System.out.println(Math.PI);
+    }
+
+}
--- src/Person.java
+++ src/Person.java
@@ -9,19 +9,32 @@ public class Person {
 
     //instance method
     public String joinChat() {
-        return lastName + " , " + firstName + " has joined the chat.";
+        return lastName + ", " + firstName + " has joined the chat.";
     }
+
+
 
 
     public static void main(String[] args) {
 
         Person nick = new Person();
+        Person ken = new Person();
+        ken.firstName = "Ken";
+        ken.lastName = "Howell";
+        ken.age = 34;
+
         nick.firstName = "Nick";
         nick.lastName = "Adams";
         nick.age = 40;
-        System.out.printf("%s %s is my name \n", nick.firstName, nick.lastName);
 
+        System.out.printf("%s %s is my name, %s is my age \n", nick.firstName, nick.lastName, nick.age);
+        System.out.printf("%s %s is my teachers name, %s is his age \n", ken.firstName, ken.lastName, ken.age);
         System.out.println(nick.joinChat());
+        System.out.println(ken.joinChat());
+
+
+
+
     }
 }
 
