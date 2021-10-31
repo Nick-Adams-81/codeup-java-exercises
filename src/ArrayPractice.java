a09cca8ce158b7d557182746f99e9a7af80d83e7
nick adams <thebigdeal916@icloud.com>
2021-10-31 11:32:56 -0500

    added for loop for two dimensional array

--- src/ArrayPractice.java
+++ src/ArrayPractice.java
@@ -42,6 +42,27 @@ public class ArrayPractice {
         System.out.println(matrix[0][2]); // 3
         // the first element in the last row
         System.out.println(matrix[2][0]); // 7
+
+
+        int[][] matrix2 = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for (int[] row : matrix2) {
+            System.out.println("+---+---+---+");
+
+            System.out.print("| ");
+
+            for (int n : row) {
+                System.out.print(n + " | ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("+---+---+---+");
     }
 
 }
