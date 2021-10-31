4291f6a2986508b23efe2d7caef648aa26892d54
nick adams <thebigdeal916@icloud.com>
2021-10-31 11:17:33 -0500

    added first couple of arrays

---
+++ src/ArrayPractice.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class ArrayPractice {
+
+    public static void main(String[] args) {
+
+        int[] numbers = new int[4];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+
+        System.out.println(Arrays.toString(numbers));
+
+        String[] names = new String[4];
+        names[0] = "Nick";
+        names[1] = "chris";
+        names[2] = "matt";
+        names[3] = "ryan";
+
+        System.out.println(Arrays.toString(names));
+    }
+
+}
