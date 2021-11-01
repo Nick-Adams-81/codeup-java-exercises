4e4b4569624e95b1f4cf834a31e3e0503ea814c0
nick adams <thebigdeal916@icloud.com>
2021-11-01 09:42:44 -0500

    refactor of morning warm up

--- src/Candy.java
+++ src/Candy.java
@@ -1,22 +1,43 @@
 import java.util.Scanner;
 public class Candy {
 
+    public String name;
+    public String size;
 
-
-    public static String candyName() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("input the candy name: ");
-        return scanner.nextLine();
+    public void talkAboutCandy() {
+        System.out.printf("Ahh, a %s! I see you got the %s size! Looks good", name, size);
     }
 
-    public static String candySize() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("input the candy size: ");
-        return scanner.nextLine();
+    public Candy(String name) {
+        this.name = name;
+        this.size = "undefined";
     }
+
+    public Candy(String name, String size) {
+        this.name = name;
+        this.size = size;
+    }
+
+
+
+
+
+
 
 
     public static void main(String[] args) {
-        System.out.println("Ahhh, a " + candyName() + "! I see that you got the " + candySize() + " size of it. Sounds good!!!");
+
+       Candy unknownCandy = new Candy("fun candy");
+       Candy newUnknownCandy = new Candy("funner candy", "Big!!!");
+
+        System.out.println(unknownCandy.name);
+        System.out.println(unknownCandy.size);
+        System.out.println(newUnknownCandy.name);
+        System.out.println(newUnknownCandy.size);
+
+        newUnknownCandy.talkAboutCandy();
+
+
+
     }
 }
