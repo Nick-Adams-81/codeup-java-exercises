eba30b1abeeb0ab3e650f9bdc44b2a27ba6f64f9
nick adams <thebigdeal916@icloud.com>
2021-11-01 11:26:19 -0500

    added matrix to array lecture

--- src/ArrayLecture.java
+++ src/ArrayLecture.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArrayLecture {
 
 
@@ -5,19 +7,20 @@ public class ArrayLecture {
 
         // declaring an array with a type
         // dataType   var name   setting array length
-            double[] prices = new double[4];
+//            double[] prices = new double[4];
+//
+//
+//        final int numberOfSides = 6;
+//        int[] diceSides = new int[numberOfSides];
+//        diceSides[0] = 1;
+//
+//        System.out.println(diceSides.length);
+//        System.out.println(prices.length);
+//        System.out.println(diceSides[0]);
 
-
-        final int numberOfSides = 6;
-        int[] diceSides = new int[numberOfSides];
-        diceSides[0] = 1;
-
-        System.out.println(diceSides.length);
-        System.out.println(prices.length);
-        System.out.println(diceSides[0]);
-
+        // directly adding items to an array
         String[] myBag = {"Snickers", "Reese's", "Brussel sprouts"};
-        System.out.println(myBag.length);
+//        System.out.println(myBag.length);
         //System.out.println(myBag[1]);
 
         // old for loop
@@ -26,11 +29,60 @@ public class ArrayLecture {
 //        }
 
         //enhanced for loop
+//        for(String candy : myBag) {
+//            System.out.println(candy);
+//        }
 
-        for(String n : myBag) {
-            System.out.println(n);
+        // arrays class
+        String[] anotherBag = new String[12];
+//        System.out.println(anotherBag[0]);
+//        System.out.println(anotherBag[5]);
+
+        // auto fill the array
+        //Arrays.fill(anotherBag, "snickers");
+
+//        for(String candy: anotherBag) {
+//           System.out.println(candy);
+//        }
+
+        // finding if two arrays are equal
+        System.out.println(Arrays.equals(anotherBag, anotherBag));
+
+        // increasing the length of an array
+        String[] mybagplustwo = Arrays.copyOf(myBag, myBag.length + 1);
+        mybagplustwo[mybagplustwo.length - 1] = "crunch bar";
+
+
+        // printing the original array
+        System.out.println(Arrays.toString(mybagplustwo));
+        // array sort method
+        Arrays.sort(mybagplustwo);
+        for(String candy : mybagplustwo) {
+            System.out.println(candy);
         }
+
+        //array to string method
+        System.out.println((Arrays.toString(mybagplustwo)));
+
+        // two-dimensional array (matrix)
+
+        int [][] matrix = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+
+        for(int[] row : matrix) {
+            System.out.println("+---+---+---+");
+            System.out.print("| ");
+            for(int n : row) {
+                System.out.print(n + " | ");
+            }
+            System.out.println();
+        }
+        System.out.println("+---+---+---+");
 
 
     }
+
 }
