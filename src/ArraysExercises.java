2ad5948933f2606255c4cd8a6505e53e93e4cb60
nick adams <thebigdeal916@icloud.com>
2021-11-01 09:01:02 -0500

    added loop to add element to array

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,7 +1,24 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class ArraysExercises {
 
+
+    public static String[] addPerson(String[] Person) {
+
+        String[] newPerson = new String[Person.length + 1];
+        for(int i = 0; i < Person.length; i++) {
+            newPerson[i] = Person[i];
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input a new name");
+        newPerson[newPerson.length - 1] = scanner.nextLine();
+        System.out.println("New person array: " + Arrays.toString(newPerson));
+
+
+        return newPerson;
+    }
 
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
@@ -12,6 +29,9 @@ public class ArraysExercises {
         Person[0] = "Nick";
         Person[1] = "Chris";
         Person[2] = "John";
+
+        System.out.println("old person array: " + Arrays.toString(Person));
+        Person = addPerson(Person);
 
         for (String name : Person) {
             System.out.println(name);
