98556dc9876eeb5c11e8bb2c96bbc864a189389f
nick adams <thebigdeal916@icloud.com>
2021-11-02 11:06:26 -0500

    added methods exclusive to extended class

--- src/Employee.java
+++ src/Employee.java
@@ -1,14 +1,28 @@
 public class Employee extends Person {
 
 
-   public Employee(String name) {
-       super(name);
+   public Employee(String employeeName) {
+       super(employeeName);
+   }
+
+   public void sayHello() {
+       System.out.println("Welcome to Skynet, how can we help you?");
+   }
+
+   public void doWork() {
+       System.out.println("work, work, work....");
    }
 
     public static void main(String[] args) {
 
+        Person unemployed = new Person("Nikki");
         Employee nick = new Employee("Nick");
 
+        unemployed.sayHello();
+        nick.sayHello();
+        System.out.println("name = " + nick.getName());
+
+        nick.doWork();
 
 
     }
