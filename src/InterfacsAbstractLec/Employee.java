03d38be845ac31a98ae426907f457c497b12d5a8
nick adams <thebigdeal916@icloud.com>
2021-11-04 10:20:14 -0500

    added getters and set up abstract string work

--- src/InterfacsAbstractLec/Employee.java
+++ src/InterfacsAbstractLec/Employee.java
@@ -2,12 +2,28 @@ package InterfacsAbstractLec;
 
 abstract class Employee {
 
+    // initializing variables
     protected String name;
     protected String department;
 
+    // employee contructor
     public Employee(String name, String department) {
         this.name = name;
         this.department = department;
     }
+
+    // getter for name
+    public String getName() {
+        return  this.name;
+    }
+
+    // getter for department
+    public String getDepartment() {
+        return this.department;
+    }
+
+    public abstract String work();
+
+
 
 }
