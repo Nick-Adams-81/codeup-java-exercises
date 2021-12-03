c69af80cddc3384cc9cae589c2291bc57ec16369
nick adams <thebigdeal916@icloud.com>
2021-12-02 20:43:38 -0600

    added removed vowels method

--- src/StuffAndThings/Algorithyms.java
+++ src/StuffAndThings/Algorithyms.java
@@ -2,20 +2,47 @@ package StuffAndThings;
 
 
 import java.util.Arrays;
+import java.util.List;
 
 public class Algorithyms {
 
 
 
+    // remove vowel from a string
+    static String removeVowel(String info) {
+        Character[] vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+        List<Character> al = Arrays.asList(vowels);
+        StringBuffer sb = new StringBuffer(info);
+        for(int i = 0; i < sb.length(); i++) {
+            if(al.contains(sb.charAt(i))) {
+                sb.replace(i, i + 1, "");
+                i--;
+            }
+        }
+
+        return sb.toString();
+
+    }
+
 
     public static void main(String[] args) {
 
         // printing the next to last word of a string
-        String str = "hello to you good sir fuck you!";
+        String str = "hello to you good sir!";
+
         String[] bits = str.split(" ");
         System.out.println(Arrays.toString(bits));
         String lastWord = bits[bits.length - 2];
         System.out.println(lastWord);
+
+
+        System.out.println(removeVowel(str));
+
+
+
+
+
     }
 
 
